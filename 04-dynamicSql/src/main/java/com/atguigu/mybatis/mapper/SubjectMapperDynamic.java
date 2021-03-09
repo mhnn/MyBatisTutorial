71696e7116d260e079a709919cef6f692bb0b788
root <yxl980411@gmail.com>
2021-03-09 15:12:00 +0800

    动态sql完结

--- 04-dynamicSql/src/main/java/com/atguigu/mybatis/mapper/SubjectMapperDynamic.java
+++ 04-dynamicSql/src/main/java/com/atguigu/mybatis/mapper/SubjectMapperDynamic.java
@@ -7,4 +7,12 @@ import java.util.List;
 public interface SubjectMapperDynamic {
     //查询一组电影信息，带了哪个字段查询条件就拼上字段的值
     List<Subject> getSubsByConditionIf(Subject subject);
+
+    List<Subject> getSubsByConditionTrim(Subject subject);
+
+    List<Subject> getSubsByConditionChoose(Subject subject);
+
+    void updateSubjectInfo(Subject subject);
+
+    List<Subject> getSubsByConditionForeach(List<Integer> typeIds);
 }
--- 04-dynamicSql/src/test/java/com/atguigu/mybatis/test/DynamicSqlTest.java
+++ 04-dynamicSql/src/test/java/com/atguigu/mybatis/test/DynamicSqlTest.java
@@ -1,6 +1,9 @@
 package com.atguigu.mybatis.test;
 
+import com.atguigu.mybatis.bean.Admin;
 import com.atguigu.mybatis.bean.Subject;
+import com.atguigu.mybatis.mapper.AdminMapper;
+import com.atguigu.mybatis.mapper.AdminMapperDynamic;
 import com.atguigu.mybatis.mapper.SubjectMapperDynamic;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -10,6 +13,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DynamicSqlTest {
@@ -32,7 +38,95 @@ public class DynamicSqlTest {
         }finally {
             openSession.close();
         }
+    }
+    @Test
+    public void testTrim() throws IOException {
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        SqlSession openSession = sqlSessionFactory.openSession();
+        try {
+            SubjectMapperDynamic mapper = openSession.getMapper(SubjectMapperDynamic.class);
+            Subject subject = new Subject();
+            subject.setsHit(100);
+            List<Subject> subs = mapper.getSubsByConditionTrim(subject);
+            for (Subject subject1:subs){
+                System.out.println(subject1);
+            }
+        } finally {
+            openSession.close();
+        }
+    }
+    @Test
+    public void testChoose() throws IOException {
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        SqlSession openSession = sqlSessionFactory.openSession();
+        try {
+            SubjectMapperDynamic mapper = openSession.getMapper(SubjectMapperDynamic.class);
+            Subject subject = new Subject();
+            List<Subject> subs = mapper.getSubsByConditionChoose(subject);
+            for (Subject subject1:subs){
+                System.out.println(subject1);
+            }
+        }finally {
+            openSession.close();
+        }
+        }
+    @Test
+    public void testSet() throws IOException {
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        SqlSession openSession = sqlSessionFactory.openSession();
+        try {
+            SubjectMapperDynamic mapper = openSession.getMapper(SubjectMapperDynamic.class);
+            Subject subject = new Subject();
+            subject.setsId(2609258);
+            subject.setsHit(200);
+            mapper.updateSubjectInfo(subject);
+            openSession.commit();
+        }finally {
+            openSession.close();
+        }
+        }
+    @Test
+    public void testForeach() throws IOException {
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        SqlSession openSession = sqlSessionFactory.openSession();
+        try {
+            SubjectMapperDynamic mapper = openSession.getMapper(SubjectMapperDynamic.class);
+            List<Subject> subs = mapper.getSubsByConditionForeach(Arrays.asList(24, 29, 25));
+            for (Subject subject1:subs){
+                System.out.println(subject1);
+            }
 
-
+        }finally {
+            openSession.close();
+        }
+        }
+    @Test
+    public void testForeachAdd() throws IOException {
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        SqlSession openSession = sqlSessionFactory.openSession();
+        try {
+            AdminMapperDynamic mapperDynamic = openSession.getMapper(AdminMapperDynamic.class);
+            List<Admin>  ads = new ArrayList<>();
+            ads.add(new Admin("tom1", "atguigu"));
+//            ads.add(new Admin("frank", "12311"));
+            mapperDynamic.addAds(ads);
+            openSession.commit();
+        }finally {
+            openSession.close();
+        }
+    }
+    @Test
+    public void testInnerParam() throws IOException {
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        SqlSession openSession = sqlSessionFactory.openSession();
+        try {
+            AdminMapperDynamic mapperDynamic = openSession.getMapper(AdminMapperDynamic.class);
+            Admin admin = new Admin();
+            admin.setaName("e");
+            List<Admin> admins = mapperDynamic.getAdsTestInnerParam(admin);
+            System.out.println(admins);
+        }finally {
+            openSession.close();
+        }
     }
 }
